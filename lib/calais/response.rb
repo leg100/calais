f96c9da879da24a0e392ec09a79c9e3f0a33ca55
Louis Garman <louisgarman@gmail.com>
2012-09-04 00:34:41 +0100

    Reveal the raw_response instance variable

--- lib/calais/response.rb
+++ lib/calais/response.rb
@@ -13,7 +13,7 @@ module Calais
     }
 
     attr_accessor :submitter_code, :signature, :language, :submission_date, :request_id, :doc_title, :doc_date
-    attr_accessor :hashes, :entities, :relations, :geographies, :categories, :socialtags, :relevances
+    attr_accessor :hashes, :entities, :relations, :geographies, :categories, :socialtags, :relevances, :raw_response
 
     def initialize(rdf_string)
       @raw_response = rdf_string
